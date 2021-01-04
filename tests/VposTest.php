89ebf8e8eda6aa71de5debd7d780a7cedf555984
Alexandre Antonio Juca <corextechnologies@gmail.com>
2021-01-04 11:54:19 +0100

    Add test case

---
+++ tests/VposTest.php
@@ -0,0 +1,17 @@
+<?php
+    declare(strict_types=1);
+
+    namespace Vpos\Test;
+
+    use PHPUnit\Framework\TestCase;
+
+
+    class VposTest extends TestCase 
+    {
+        public function testItShouldGetTransactions() 
+        {
+            $this->assertEquals(1, 0);
+        }
+    }
+
+?>
