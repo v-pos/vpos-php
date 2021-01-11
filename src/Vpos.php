f8e77cb5f4fd3ceeed5a9e6d2f55f91fff82ee56
Alexandre Antonio Juca <corextechnologies@gmail.com>
2021-01-11 09:07:10 +0100

    refactor code

--- src/Vpos.php
+++ src/Vpos.php
@@ -4,11 +4,11 @@
     namespace Vpos\Vpos;
 
     use GuzzleHttp\Client;
-    use GuzzleHttp\RequestOptions;
     use Ramsey\Uuid\Uuid;
 
     final class Vpos
     {
+        const LOCATION = 17;
         private $host;
         private $pos_id;
         private $refund_callback_url;
@@ -16,7 +16,6 @@
         private $supervisor_card;
         private $token;
         private $client;
-        const LOCATION = 17;
 
         public function __construct() 
         {
@@ -26,62 +25,63 @@
             $this->payment_callback_url = $this->getPaymentCallbackUrl();
             $this->supervisor_card = $this->getSupervisorCard();
             $this->token = $this->getMerchantToken();
-            $this->client = new \GuzzleHttp\Client();
+            $this->client = new Client();
         }
 
-        public function getTransactions() 
+        private function getHost()
+        {
+            if (getenv("VPOS_ENVIRONMENT") == "PRD")
+            {
+                return "https://api.vpos.ao/api/v1";
+            } else {
+                return "https://sandbox.vpos.ao/api/v1";
+            }
+        }
+
+        private function getPosId()
+        {
+            return (int) getenv("GPO_POS_ID");
+        }
+
+        private function getRefundCallbackUrl()
+        {
+            return getenv("VPOS_REFUND_CALLBACK_URL");
+        }
+
+        private function getPaymentCallbackUrl()
+        {
+            return getenv("VPOS_PAYMENT_CALLBACK_URL");
+        }
+
+        private function getSupervisorCard()
+        {
+            return getenv("GPO_SUPERVISOR_CARD");
+        }
+
+        private function getMerchantToken()
+        {
+            return "Bearer " . getenv("MERCHANT_VPOS_TOKEN");
+        }
+
+        public function getTransactions()
         {
             $response = $this->client->request('GET', $this->host . "/transactions", $this->setDefaultRequestOptions());
             return $this->returnVposObject($response);
         }
 
-        public function getTransaction($id)
+        private function setDefaultRequestOptions()
         {
-            $response = $this->client->request("GET", $this->host . "/transactions/" . $id, $this->setDefaultRequestOptions());
-            return $this->returnVposObject($response);
+            return [
+                'http_errors' => false,
+                ['allow_redirects' => false],
+                'headers' => [
+                'Authorization' => $this->token,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json']
+            ];
         }
 
-        public function newPayment($customer, $amount)
-        {
-            $options = $this->setRequestOptionsForPayment(
-                customer: $customer,
-                amount: $amount,
-                transaction_type: "payment"
-            );
-            $response = $this->client->request("POST", $this->host . "/transactions", $options);
-            return $this->returnVposObject($response);
-        }
-
-        public function newRefund($id)
-        {
-            $options = $this->setRequestOptionsForRefund(
-                transaction_id: $id,
-                transaction_type: "refund"
-            );
-            $response = $this->client->request("POST", $this->host . "/transactions", $options);
-            return $this->returnVposObject($response);
-        }
-
-        public function getRequest($id)
-        {
-            $options = $this->setDefaultRequestOptions();
-            $response = $this->client->request("GET", $this->host . "/requests/" . $id, $options);
-            return $this->returnVposObject($response);
-        }
-
-        public function getRequestId($response)
-        {
-            if ($response['status'] == 202) {
-                return substr($response['location'], self::LOCATION);
-            }
-        }
-
-        public function setToken($token): void 
-        {
-            $this->token = "Bearer ". $token;
-        }
-
-        private function returnVposObject($response) 
+        private function returnVposObject($response)
         {
             switch($response->getStatusCode()) {
                 case 200:
@@ -102,8 +102,33 @@
                     'message' => $response->getReasonPhrase(),
                     'details' => $response->getBody()->getContents()
                 ];
-                    
+
             }
+        }
+
+        public function getTransaction($id)
+        {
+            $response = $this->client->request(
+                "GET",
+                $this->host . "/transactions/" . $id,
+                $this->setDefaultRequestOptions()
+            );
+            return $this->returnVposObject($response);
+        }
+
+        public function newPayment($customer, $amount)
+        {
+            $options = $this->setRequestOptionsForPayment(
+                customer: $customer,
+                amount: $amount,
+                transaction_type: "payment"
+            );
+            $response = $this->client->request(
+                "POST",
+                $this->host . "/transactions",
+                $options)
+            ;
+            return $this->returnVposObject($response);
         }
 
         private function setRequestOptionsForPayment($customer, $amount, $transaction_type)
@@ -127,71 +152,61 @@
             ];
         }
 
-            private function setRequestOptionsForRefund($transaction_id, $transaction_type)
-            {
-                return [
-                    'http_errors' => false,
-                    ['allow_redirects' => false],
-                    'json' => [
-                        'type' => $transaction_type,
-                        'parent_transaction_id' => $transaction_id,
-                        'supervisor_card' => $this->supervisor_card,
-                        'callback_url' => $this->refund_callback_url
-                    ],
-                    'headers' => [
-                        'Idempotency-Key' => Uuid::uuid4()->toString(),
-                        'Authorization' => $this->token,
-                        'Content-Type' => 'application/json',
-                        'Accept' => 'application/json'
-                    ]
-                ];
-            }
+        public function newRefund($id)
+        {
+            $options = $this->setRequestOptionsForRefund(
+                transaction_id: $id,
+                transaction_type: "refund"
+            );
+            $response = $this->client->request(
+                "POST",
+                $this->host . "/transactions",
+                $options
+            );
+            return $this->returnVposObject($response);
+        }
 
-        private function setDefaultRequestOptions()
+        private function setRequestOptionsForRefund($transaction_id, $transaction_type)
         {
             return [
                 'http_errors' => false,
                 ['allow_redirects' => false],
+                'json' => [
+                    'type' => $transaction_type,
+                    'parent_transaction_id' => $transaction_id,
+                    'supervisor_card' => $this->supervisor_card,
+                    'callback_url' => $this->refund_callback_url
+                ],
                 'headers' => [
-                'Authorization' => $this->token,
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json']
+                    'Idempotency-Key' => Uuid::uuid4()->toString(),
+                    'Authorization' => $this->token,
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json'
+                ]
             ];
         }
 
-        private function getPosId() 
+        public function getRequest($id)
         {
-            return (int) getenv("GPO_POS_ID");
+            $options = $this->setDefaultRequestOptions();
+            $response = $this->client->request(
+                "GET",
+                $this->host . "/requests/" . $id,
+                $options
+            );
+            return $this->returnVposObject($response);
         }
 
-        private function getRefundCallbackUrl()
+        public function getRequestId($response)
         {
-            return getenv("VPOS_REFUND_CALLBACK_URL");
-        }
-
-        private function getPaymentCallbackUrl()
-        {
-            return getenv("VPOS_PAYMENT_CALLBACK_URL");
-        }
-
-        private function getSupervisorCard() 
-        {
-            return getenv("GPO_SUPERVISOR_CARD");
-        }
-
-        private function getMerchantToken() 
-        {
-            return "Bearer " . getenv("MERCHANT_VPOS_TOKEN");
-        }
-
-        private function getHost() 
-        {
-            if (getenv("VPOS_ENVIRONMENT") == "PRD")
-            {
-                return "https://api.vpos.ao/api/v1";
-            } else {
-                return "https://sandbox.vpos.ao/api/v1";
+            if ($response['status'] == 202) {
+                return substr($response['location'], self::LOCATION);
             }
+        }
+
+        public function setToken($token): void
+        {
+            $this->token = "Bearer ". $token;
         }
     }
 
